0f43f53dd04885628749e18e0f7b05817b351c25
David Pastl <david.pastl@usask.ca>
2015-12-03 18:00:19 -0600

    Added some default files

---
+++ code/databases/database_template.py
@@ -0,0 +1,6 @@
+#
+# A simple template for the database class that demonstrates how to generate a database file
+#
+# Naming convention:
+#   datebase filenames will have the form "db_<nameofdatabase>.py" so that
+#   the scripts can easily determine what is a database and what isn't.
