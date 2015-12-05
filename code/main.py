1fa734ff6250ae8a1864fe15055f359f7f078de4
David Pastl <david.pastl@usask.ca>
2015-12-05 15:37:41 -0600

    main file now runs the exam from exam.py.  We really don't need main right now but we'll likely want it in the future.

--- code/main.py
+++ code/main.py
@@ -1,41 +1,7 @@
 '''
-Main console application.  Handles user input and such but not the generation or getting of questions
+Main application.  Does any setup required, including starting a GUI.
 '''
-from database_accessor import DatabaseAccessor
+from exam import Exam
 
-# create the question handler then just loop until user wants to exit or the exam is done
-
-print "Welcome to the Glider Flight Test Exam Generator"
-for _ in range(3):
-    try:
-        num_questions = int(raw_input("Please enter the number of questions you'd like (1-100):"))
-        break
-    except:
-        print "Sorry, that number was invalid, try again"
-
-database = DatabaseAccessor()
-
-for i in range(num_questions):
-    print "Question #{}:".format(i+1)
-
-    # get question1
-    question = database.get_random_question()
-    # display question
-    print question
-
-    # wait for user input
-    try:
-        # Need a try-except to catch things that aren't integers, otherwise we don't catch the exception int throws
-        user_answer = int(raw_input("Selection: ")) - 1
-        # check user input and print a response
-        if question.check_answer(user_answer):
-            question.print_answer_text()
-        else:
-            print "Incorrect."
-
-        #Print a space to separate questions.
-        print ""
-    except:
-        print "Valid integers only please"
-
-    pass
+exam = Exam()
+exam.administer_exam()
