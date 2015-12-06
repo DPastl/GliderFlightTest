823040ca74f40010a64a05574c3dc1d0bb4b6c68
Ashton Seth Reimer <ashtonsethreimer@gmail.com>
2015-12-06 01:18:32 -0600

    Added comments and combined methods.

--- code/database_accessor.py
+++ code/database_accessor.py
@@ -1,26 +1,36 @@
 '''
-A class that handles the getting a setting of questions in the databases.
+A class that handles the getting and setting of questions in the databases.
 
 Loads the databases and then automagically will provide questions as requested
 '''
-import json
-from random import randrange, choice
-
 from question_class import QuestionClass
 
+# The relative path to the root directory containing the .json
+# question database files.
 database_root_dir = 'databases'
+# A dict() mapping the type of database to the appropriate .json
+# database file. There are 8 sections of topics to study for. See
+# the TC Study and Reference Guide.
 database_file_enum = {
     'airlaw': 'db_airlaw.json',
-    'flight_theory': 'db_flight_theory.json',
+    'navigation': 'db_navigation.json',
     'metrology': 'db_metrology.json',
-    'navigation': 'db_navigation.json'
+    'flight_theory': 'db_flight_theory.json',
+    'instruments': 'db_instruments.json',
+    'flight_operations': 'db_flight_operations.json',
+    'human_factors': 'db_human_factors.json',
+
 }
 
+
+# A class to handle parsing pulling questions from the database files.
 class DatabaseAccessor(object):
     database_list = {}
     allow_duplicates = False
 
     def __init__(self, database_to_use='all', allow_duplicates=False):
+        import json
+
         self.allow_duplicates = allow_duplicates
         if database_to_use == 'all':
             for database_name, database_file_name in database_file_enum.iteritems():
@@ -33,28 +43,25 @@ class DatabaseAccessor(object):
                 with open(database_root_dir + '/' + database_file_enum[database_to_use], 'r') as f:
                     self.database_list[database_to_use] = json.load(f)
 
-    def get_random_question(self):
+
+    # This method randomly selects a question from a database.
+    def get_random_question(self,db_name=None):
         # We need to add something that keeps track of which questions we've provided so we don't have duplicates.
         # or just not care that there might be duplicates
-        random_database = choice(database_file_enum.keys())
-        num_questions = len(self.database_list[random_database])
-        rand_index = randrange(num_questions)
-        return QuestionClass(self.database_list[random_database][rand_index])
+        # OR this can be handled by the Exam class.
+        from random import choice
 
-    def get_random_question_from(self, database_name):
-        if database_name not in database_file_enum.keys():
-            raise Exception("Database name not in list!")
-        num_questions = len(self.database_list[database_name])
-        rand_index = randrange(num_questions)
-        return QuestionClass(self.database_list[database_name][rand_index])
+        # If database name is supplied, verify the name
+        if db_name is not None:
+            if db_name not in database_file_enum.keys():
+                raise Exception("Database name not in list!")
 
-    def get_question(self, database_name, question_name):
-        # Even more not sure how this will work
-        pass
+        # If not specific database name is supplied, just grab
+        # questions from a randomly chosen database.
+        if db_name is None:
+            db_name = choice(database_file_enum.keys())
 
-    def add_question(self, database_name):
-        # Is there even a point to this?
-        pass
+        return QuestionClass(choice(self.database_list[db_name]))
 
 
 if __name__ == '__main__':
